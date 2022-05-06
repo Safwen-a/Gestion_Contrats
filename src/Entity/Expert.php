9a4f04cf9bd98abd44c6fccf4c1b54f0eb4df28d
Hamzuss <96492210+Hamzuss@users.noreply.github.com>
2022-05-06 21:22:48 +0100

    adding name&other attributes to expert&Fin

--- src/Entity/Expert.php
+++ src/Entity/Expert.php
@@ -19,6 +19,15 @@ class Expert extends User
     #[ORM\Column(type: 'integer')]
     private $Nombre_H_Fait;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $FirstName;
+
+    #[ORM\Column(type: 'integer')]
+    private $Tel;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +53,42 @@ class Expert extends User
     public function setNombreHFait(int $Nombre_H_Fait): self
     {
         $this->Nombre_H_Fait = $Nombre_H_Fait;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->FirstName;
+    }
+
+    public function setFirstName(string $FirstName): self
+    {
+        $this->FirstName = $FirstName;
+
+        return $this;
+    }
+
+    public function getTel(): ?int
+    {
+        return $this->Tel;
+    }
+
+    public function setTel(int $Tel): self
+    {
+        $this->Tel = $Tel;
 
         return $this;
     }
--- src/Entity/Financier.php
+++ src/Entity/Financier.php
@@ -13,8 +13,53 @@ class Financier extends User
     #[ORM\Column(type: 'integer')]
     private $id;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $FirstName;
+
+    #[ORM\Column(type: 'integer')]
+    private $Tel;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->FirstName;
+    }
+
+    public function setFirstName(string $FirstName): self
+    {
+        $this->FirstName = $FirstName;
+
+        return $this;
+    }
+
+    public function getTel(): ?int
+    {
+        return $this->Tel;
+    }
+
+    public function setTel(int $Tel): self
+    {
+        $this->Tel = $Tel;
+
+        return $this;
     }
 }
