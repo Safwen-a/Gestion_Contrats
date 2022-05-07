81fc866978a9f9d831ce4e42cb7f3ce6c33d907a
Hamzuss <96492210+Hamzuss@users.noreply.github.com>
2022-05-07 11:43:13 +0100

    creation fiches intervention entity

---
+++ src/Entity/FicheIntervention.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FicheInterventionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FicheInterventionRepository::class)]
+class FicheIntervention
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Nom_Intervention;
+
+    #[ORM\Column(type: 'date')]
+    private $Date_Intervention;
+
+    #[ORM\Column(type: 'array')]
+    private $Type_Intervention = [];
+
+    #[ORM\Column(type: 'integer')]
+    private $Nombre_H_realisé;
+
+    #[ORM\Column(type: 'text')]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Lieu_Intervention;
+
+    #[ORM\ManyToOne(targetEntity: Expert::class, inversedBy: 'Fiches_Intervention')]
+    private $Expert;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomIntervention(): ?string
+    {
+        return $this->Nom_Intervention;
+    }
+
+    public function setNomIntervention(string $Nom_Intervention): self
+    {
+        $this->Nom_Intervention = $Nom_Intervention;
+
+        return $this;
+    }
+
+    public function getDateIntervention(): ?\DateTimeInterface
+    {
+        return $this->Date_Intervention;
+    }
+
+    public function setDateIntervention(\DateTimeInterface $Date_Intervention): self
+    {
+        $this->Date_Intervention = $Date_Intervention;
+
+        return $this;
+    }
+
+    public function getTypeIntervention(): ?array
+    {
+        return $this->Type_Intervention;
+    }
+
+    public function setTypeIntervention(array $Type_Intervention): self
+    {
+        $this->Type_Intervention = $Type_Intervention;
+
+        return $this;
+    }
+
+    public function getNombreHRealisé(): ?int
+    {
+        return $this->Nombre_H_realisé;
+    }
+
+    public function setNombreHRealisé(int $Nombre_H_realisé): self
+    {
+        $this->Nombre_H_realisé = $Nombre_H_realisé;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLieuIntervention(): ?string
+    {
+        return $this->Lieu_Intervention;
+    }
+
+    public function setLieuIntervention(string $Lieu_Intervention): self
+    {
+        $this->Lieu_Intervention = $Lieu_Intervention;
+
+        return $this;
+    }
+
+    public function getExpert(): ?Expert
+    {
+        return $this->Expert;
+    }
+
+    public function setExpert(?Expert $Expert): self
+    {
+        $this->Expert = $Expert;
+
+        return $this;
+    }
+}
